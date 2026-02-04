e675df879f770cd5edb8222159485fc2898836a0
LesMrCode <mhonelesleyeric@gmail.com>
2026-02-04 21:17:46 +0200

    fixed imports

--- notes/src/components/DashboardContent.jsx
+++ notes/src/components/DashboardContent.jsx
@@ -3,8 +3,8 @@
 import { useState, useEffect } from "react"
 import NoteEditor from "./NoteEditor"
 import NoteList from "./NoteList"
-import { Button } from "../components/ui/button"
-import { Card, CardContent } from "./ui/card"
+import { Button } from "../components/ui/button.jsx"
+import { Card, CardContent } from "./ui/card.jsx"
 
 
 
