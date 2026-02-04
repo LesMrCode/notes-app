62fe54ca871c4f02b01b02e1fd8b76fba73c9b13
LesMrCode <mhonelesleyeric@gmail.com>
2026-02-04 16:57:02 +0200

    updates

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import DashboardContent from "./components/DashboardContent"
-import Login from "./pages/Login.jsx"
-import Register from "./pages/Register.jsx" 
+import Login from "./pages/Login"
+import Register from "./pages/Register" 
 import { Routes, Route} from "react-router-dom"
 
 
